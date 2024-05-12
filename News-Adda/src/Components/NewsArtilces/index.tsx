58e9e960a363aeda4234314a6db1d506512c2c20
kihan2518B <kp648027@gmail.com>
2024-05-12 16:04:26 +0530

    enhanced ui responsiveness

--- News-Adda/src/Components/NewsArtilces/index.tsx
+++ News-Adda/src/Components/NewsArtilces/index.tsx
@@ -9,7 +9,7 @@ const NewsArticles = () => {
     const { theme } = useContext(ThemeContext)
     const [state, dispatch] = useReducer(reducer, initialState)
     const { isLoading, isError, errorMessage, articles } = state
-    console.log("articles: ", articles);
+    // console.log("articles: ", articles);
 
     useEffect(() => {
         fetchArticles(dispatch)
@@ -26,7 +26,7 @@ const NewsArticles = () => {
         // Parse dates from strings
         const dateA = new Date(a.date);
         const dateB = new Date(b.date);
-    
+
         // Compare dates first
         if (dateA.getDate() !== dateB.getDate()) {
             return dateB.getDate() - dateA.getDate();
@@ -44,19 +44,19 @@ const NewsArticles = () => {
 
     //this will convert all element of set to string explicitly 
     const categories: string[] = Array.from(categoriesSet).map(String);
-    console.log("categories", categories)
+    // console.log(categories)
 
     return (
         <>
             <TabGroup>
-                <div className={`News-Articles h-full w-full ${theme == 'dark' ? "bg-violet-500  text-white border-violet-950" : "bg-violet-300 border-violet-900 hover:bg-violet-200"} border-0 rounded-t-xl`}>
+                <div className={`News-Articles  md:h-[90vh] w-full ${theme == 'dark' ? "bg-violet-500  text-white " : "bg-violet-400 "}  rounded-xl`}>
                     <div id="heading-section" className="h-12 w-full ">
                         <div className={` h-full w-full flex items-center justify-evenly text-xl max-[1200px]:text-lg max-[1000px]:text-base max-[835px]:text-xs`}>
-                            <TabList className="flex gap-4">
+                            <TabList className="flex gap-4 overflow-x-auto">
                                 {categories.map((category) => (
                                     < Tab
                                         key={category}
-                                        className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
+                                        className="rounded-full py-1 px-3 text-sm font-semibold text-black focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
                                     >
                                         {category}
                                     </Tab>
@@ -67,7 +67,7 @@ const NewsArticles = () => {
                     <TabPanels >
                         {categories.map((category) => (
                             <TabPanel key={category}>
-                                <div id="Articles" className={` h-[70vh] w-full overflow-y-auto`}>
+                                <div id="Articles" className={`h-[70vh] md:h-[80vh] w-full overflow-y-auto flex flex-col gap-3`}>
                                     {articles.filter((article) => article.sport.name === category).map((article) => (
                                         <div key={article.id} className={` mx-auto flex gap-2 ${theme == 'dark' ? "bg-violet-700 hover:bg-violet-600 text-white border-violet-950" : "bg-violet-300 border-violet-900 hover:bg-violet-200"} h-[15vw] max-[651px]:h-[16vw] max-[592px]:h-[18vw] max-[581px]:h-[20vw] max-[545px]:h-[22vw] max-[500px]:h-[26vw] max-[421px]:h-[28vw] max-[391px]:h-[32vw] max-[381px]:h-[34vw] max-[353px]:h-[36vw] max-[321px]:h-[38vw] w-[98%] border `}>
                                             <div className="h-full w-[70%] flex flex-col">
--- News-Adda/src/Components/TrendingNews/index.tsx
+++ News-Adda/src/Components/TrendingNews/index.tsx
@@ -5,18 +5,18 @@ import { ThemeContext } from "../../context/theme"
 const TrendingNews = () => {
     const { theme } = useContext(ThemeContext)
     return (
-        <>
-            <h1 className="py-2 ml-10 text-2xl font-semibold">Trending News</h1>
-            <div className={`flex md:flex-row ${theme == 'dark' ? 'dark' : ''} flex-col w-[95%] mx-auto`}>
-                <div className={` border-r-2 ${theme == 'dark' ? 'border-purple-500 ' : 'border-gray-800'} md:w-2/3 w-full flex-grow sm:col-span-1 min-h-96 h-full`}>
+        <div>
+            <h1 className={`pl-10 h-12 w-full  ${theme == "dark" ? "text-violet-50" : "text-violet-800"} flex justify-start items-end font-bold text-2xl`}>Trending News</h1>
+            <div className={`flex h-full gap-3 md:flex-row ${theme == 'dark' ? 'dark' : ''} flex-col w-[95%] mx-auto`}>
+                <div className={`${theme == 'dark' ? ' shadow-violet-600' : 'shadow-violet-400 '} shadow-md rounded-xl md:w-2/3 w-full h-full flex-grow sm:col-span-1`}>
                     <NewsArticles />
                 </div>
-                <div className=" w-full md:w-1/3 flex-grow sm:col-span-1 min-h-96">
-                    {/* <NewsArticles /> */}
+                <div className="bg-red-500 w-full md:w-1/3 flex-grow sm:col-span-1 min-h-96 rounded-md">
+                    {/* Favourites */}
                 </div>
             </div>
 
-        </>
+        </div>
     )
 }
 
